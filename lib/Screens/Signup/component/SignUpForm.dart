a99cbdf32fa023cd7f059556bb2544282ebc491c
Avinash Mali <avi24.9@gmail.com>
2020-08-27 17:07:37 +0530

    27-08-2020 changes

--- lib/Screens/Signup/component/SignUpForm.dart
+++ lib/Screens/Signup/component/SignUpForm.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:intl/intl.dart';
 import 'package:leads_in/Palette.dart';
+import 'package:leads_in/Screens/screens.dart';
+import 'package:leads_in/assets.dart';
 
 class SignUpForm extends StatelessWidget {
   SignUpForm({
@@ -31,79 +34,215 @@ class MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
+    final registerButton = MaterialButton(
+      height: 50.0,
+      minWidth: 150.0,
+      shape: buttonBorder,
+      //minWidth: MediaQuery.of(context).size.width,
+      color: PrimaryColor,
+      textColor: Colors.white,
+      padding: buttonPadding,
+      onPressed: () {
+        if (_fbKey.currentState.saveAndValidate()) {
+          print(_fbKey.currentState.value);
+        } else {
+          print(_fbKey.currentState.value);
+          print('validation failed');
+          print('Sign Up');
+        }
+      },
+      child: Text(
+        "Sign Up",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+
     return Container(
-        child: Padding(
-            padding: new EdgeInsets.only(
-                left: size.width * 0.10,
-                right: size.width * 0.10,
-                top: size.height * 0.08,
-                bottom: size.height * 0.08),
-            child: Column(children: <Widget>[
-              Align(
-                alignment: Alignment.centerLeft,
-                child: RichText(
-                  text: TextSpan(
-                      text: "Username / Email ID",
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w200,
-                        color: PrimaryColor,
-                      )),
-                ),
-              ),
-              FormBuilderTextField(
-                maxLines: 1,
-                obscureText: false,
-                attribute: 'email',
-                validators: [
-                  FormBuilderValidators.required(),
-                ],
-                onChanged: (value) => print(value),
-                valueTransformer: (value) => value.toString().trim(),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: RichText(
-                  text: TextSpan(
-                      text: "Password",
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w200,
-                        color: PrimaryColor,
-                      )),
-                ),
-              ),
-              FormBuilderTextField(
-                maxLines: 1,
-                obscureText: true,
-                attribute: 'password',
-                validators: [
-                  FormBuilderValidators.required(),
-                ],
-                onChanged: (value) => print(value),
-                valueTransformer: (value) => value.toString().trim(),
-              ),
-              Row(
-                children: <Widget>[
-                  MaterialButton(
-                    color: Theme.of(context).accentColor,
-                    child: Text(
-                      'Submit',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      if (_fbKey.currentState.saveAndValidate()) {
-                        print(_fbKey.currentState.value);
-                      } else {
-                        print(_fbKey.currentState.value);
-                        print('validation failed');
-                      }
-                    },
+      child: FormBuilder(
+          // context,
+          key: _fbKey,
+          child: Padding(
+              padding: new EdgeInsets.only(
+                  left: size.width * 0.10,
+                  right: size.width * 0.10,
+                  top: size.height * 0.05,
+                  bottom: size.height * 0.05),
+              child: Column(children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Username / Email ID",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w200,
+                          color: PrimaryColor,
+                        )),
                   ),
-                ],
-              ),
-            ])));
+                ),
+                FormBuilderTextField(
+                  maxLines: 1,
+                  obscureText: false,
+                  attribute: 'email',
+                  validators: [
+                    FormBuilderValidators.required(),
+                  ],
+                  onChanged: (value) => print(value),
+                  valueTransformer: (value) => value.toString().trim(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Password",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w200,
+                          color: PrimaryColor,
+                        )),
+                  ),
+                ),
+                FormBuilderTextField(
+                  maxLines: 1,
+                  obscureText: true,
+                  attribute: 'password',
+                  validators: [
+                    FormBuilderValidators.required(),
+                  ],
+                  onChanged: (value) => print(value),
+                  valueTransformer: (value) => value.toString().trim(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Or Sign Up Using",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w200,
+                          color: PrimaryColor,
+                        )),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      MaterialButton(
+                        minWidth: 24,
+                        height: 24,
+                        onPressed: () => {print("Apple")},
+                        padding: EdgeInsets.all(0.0),
+                        child: Image.asset(
+                          Assets.appleLogo,
+                          width: 24,
+                          height: 24,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      MaterialButton(
+                        minWidth: 24,
+                        height: 24,
+                        onPressed: () => {print("Google")},
+                        padding: EdgeInsets.all(0.0),
+                        child: Image.asset(
+                          Assets.googleLogo,
+                          width: 24,
+                          height: 24,
+                          fit: BoxFit.cover,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ]),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    registerButton,
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "Already Have Account? ",
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w200,
+                                color: PrimaryColor,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Login",
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w500,
+                                color: PrimaryColor,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.of(context)
+                                      .push(_createLoginRoute());
+                                },
+                            )
+                          ],
+                        ),
+                      ),
+                    ]),
+              ]))),
+    );
+  }
+
+  final buttonPadding =
+      new EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0);
+
+  final buttonBorder = new RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(25.0),
+    side: BorderSide(color: Colors.white, width: 2.0),
+  );
+
+  Route _createLoginRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => LoginScreen(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
   }
 }
