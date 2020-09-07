b95f0615880d91afaf17d92cf84862ca2ee4bbb6
Avinash Mali <avi24.9@gmail.com>
2020-09-07 14:19:23 +0530

    07-09-2020 changes

--- lib/Screens/Profile/component/body.dart
+++ lib/Screens/Profile/component/body.dart
@@ -1,6 +1,7 @@
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:leads_in/Palette.dart';
 import 'package:leads_in/assets.dart';
 import 'package:leads_in/widgets/CustomAppBar.dart';
 
@@ -74,7 +75,7 @@ class ProfileListState extends State<ProfileList> {
         child: Stack(
           children: <Widget>[
             Positioned(
-              top: 65,
+              top: 110,
               bottom: 0,
               left: 0,
               right: 0,
@@ -93,6 +94,15 @@ class ProfileListState extends State<ProfileList> {
               alignment: Alignment.topCenter,
               child: Column(
                 children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    alignment: Alignment.centerRight,
+                    child: Image.asset(
+                      Assets.profile,
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
                   CircularProfileAvatar(
                     '',
                     child: Image.asset(
@@ -150,73 +160,253 @@ class ProfileListState extends State<ProfileList> {
                   Expanded(
                     child: ListView(
                       children: [
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Reporting Officer :",
-                                    style: TextStyle(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300,
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                            bottom: 20,
+                            left: 30,
+                            right: 30,
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: "Reporting Officer :",
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          color: Colors.grey,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Officer Name",
-                                    style: TextStyle(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300,
+                                  Expanded(
+                                    flex: 1,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: "Officer Name",
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          color: Colors.blue,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Office Location :",
-                                    style: TextStyle(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300,
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: "Office Location :",
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          color: Colors.grey,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                    text: "Officer Name",
-                                    style: TextStyle(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w300,
+                                  Expanded(
+                                    flex: 1,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: "Location Address",
+                                        style: TextStyle(
+                                          fontFamily: 'Poppins',
+                                          color: Colors.blue,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
                                     ),
                                   ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          thickness: 20,
+                          color: Colors.grey[100],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                            bottom: 20,
+                            left: 30,
+                            right: 30,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Email ID :",
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        color: Colors.grey,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ),
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "email@example.com",
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        color: Colors.blue,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Mobile Number :",
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        color: Colors.grey,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ),
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "23612836",
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        color: Colors.blue,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2,
+                          color: Colors.grey[100],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                            bottom: 20,
+                            left: 30,
+                            right: 30,
+                          ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 2.0),
+                                        child: Image.asset(
+                                          Assets.profile,
+                                          width: 24,
+                                          height: 24,
+                                        )),
+                                  ),
+                                  TextSpan(
+                                    text: "Emergency Contact",
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: PrimaryColor,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Divider(
+                          thickness: 20,
+                          color: Colors.grey[100],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 20,
+                            bottom: 20,
+                            left: 30,
+                            right: 30,
+                          ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: RichText(
+                              text: TextSpan(
+                                text: "Change Password",
+                                style: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  color: PrimaryColor,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
                                 ),
-                              ],
+                              ),
                             ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Divider(
-                              thickness: 20,
-                              color: Colors.grey[100],
-                            ),
-
-                          ],
+                          ),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  MaterialButton(
+                    height: 50.0,
+                    minWidth: 150.0,
+                    shape: buttonBorder,
+                    //minWidth: MediaQuery.of(context).size.width,
+                    color: PrimaryColor,
+                    textColor: Colors.white,
+                    padding: buttonPadding,
+                    onPressed: () {
+                      print("Log out");
+                    },
+                    child: Text(
+                      "Log Out",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
               //Image
@@ -226,4 +416,12 @@ class ProfileListState extends State<ProfileList> {
       ),
     );
   }
+
+  final buttonPadding =
+      new EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0);
+
+  final buttonBorder = new RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(25.0),
+    side: BorderSide(color: Colors.white, width: 0.0),
+  );
 }
